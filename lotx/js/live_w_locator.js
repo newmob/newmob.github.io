096378bdac91eb73cba28db3e4cae3c4eb478d2b
Marcelo de Araujo Maximiano <marcelo.maximiano@gmail.com>
2018-10-26 09:00:57 -0300

    v1.3

--- lotx/js/live_w_locator.js
+++ lotx/js/live_w_locator.js
@@ -248,6 +248,7 @@ $(function() {
             if (isValidBarcode(code)) {
                 if (hashTable.search(code)==null) {
                     hashTable.add(code, 1);
+                    alert("Adicionado");
                 } else {
                     //window.navigator.vibrate(200);
                     window.navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]);
