0d977ebf7e839515363a949607830fdf29333b64
newmob <bitspark@gmail.com>
2018-11-09 11:33:05 -0200

    v1.2

---
+++ loja/js/produto.js
@@ -0,0 +1,9 @@
+var inputTamanho = document.querySelector("[name=tamanho]");
+var outputTamanho = document.querySelector("[name=valortamanho");
+
+function mostraTamanho() {
+    outputTamanho.innerHTML = inputTamanho.value;
+}
+
+// atribui o ponteiro da função
+inputTamanho.oninput = mostraTamanho;
