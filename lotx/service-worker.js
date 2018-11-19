7df41f210346aaf856c8d837af73e20d0e0bce23
newmob <bitspark@gmail.com>
2018-11-19 00:02:41 -0200

    v2.0
    versão final para feira de inovação

--- lotx/service-worker.js
+++ lotx/service-worker.js
@@ -1,9 +1,50 @@
 var dataCacheName = 'lotxData-v1';
 var cacheName = 'lotxPWA-final-1';
 var filesToCache = [
+  '/',
+  'https://newmob.github.io/lotx/index.html',
+  'https://newmob.github.io/lotx/lerbilhete.html',
+  'https://newmob.github.io/lotx/js/adapter-latest.js',
   'https://newmob.github.io/lotx/js/app.js',
+  'https://newmob.github.io/lotx/js/extract.js',
+  'https://newmob.github.io/lotx/js/itemslide.min.js',
+  'https://newmob.github.io/lotx/js/jquery-1.11.0.min.js',
+  'https://newmob.github.io/lotx/js/jquery-3.3.1.js',
+  'https://newmob.github.io/lotx/js/jquery.mousewheel.min.js',
   'https://newmob.github.io/lotx/js/jquery.qrcode.min.js',
-  'https://newmob.github.io/lotx/js/jquery-3.3.1.js'
+  'https://newmob.github.io/lotx/js/live_w_locator.js',
+  'https://newmob.github.io/lotx/js/quagga.min.js',
+  'https://newmob.github.io/lotx/js/scale.fix.js',
+  'https://newmob.github.io/lotx/js/sliding.js',
+  'https://newmob.github.io/lotx/css/example.css',
+  'https://newmob.github.io/lotx/css/lerbilhete.css',
+  'https://newmob.github.io/lotx/css/main.css',
+  'https://newmob.github.io/lotx/css/pygment_trac.css',
+  'https://newmob.github.io/lotx/css/reset.css',
+  'https://newmob.github.io/lotx/css/sliding.css',
+  'https://newmob.github.io/lotx/css/style.css',
+  'https://newmob.github.io/lotx/css/styles.css',
+  'https://newmob.github.io/lotx/img/favicon.ico',
+  'https://newmob.github.io/lotx/img/icon-180x180.png',
+  'https://newmob.github.io/lotx/img/icon-barcode.png',
+  'https://newmob.github.io/lotx/img/icon-close.png',
+  'https://newmob.github.io/lotx/img/icon-diadesorte.png',
+  'https://newmob.github.io/lotx/img/icon-duplasena.png',
+  'https://newmob.github.io/lotx/img/icon-loteca.png',
+  'https://newmob.github.io/lotx/img/icon-lotofacil.png',
+  'https://newmob.github.io/lotx/img/icon-lotogol.png',
+  'https://newmob.github.io/lotx/img/icon-lotomania.png',
+  'https://newmob.github.io/lotx/img/icon-megasena.png',
+  'https://newmob.github.io/lotx/img/icon-quina.png',
+  'https://newmob.github.io/lotx/img/icon-resultado.png',
+  'https://newmob.github.io/lotx/img/icon-timemania.png',
+  'https://newmob.github.io/lotx/data/780706100091344678939682011813.json',
+  'https://newmob.github.io/lotx/data/780709341265343281301447000802.json',
+  'https://newmob.github.io/lotx/data/780719712414062252264406014098.json',
+  'https://newmob.github.io/lotx/data/780732693343505383339387011583.json',
+  'https://newmob.github.io/lotx/data/780748153025420691160273017346.json',
+  'https://newmob.github.io/lotx/data/780751136291420607239275001888.json',
+  'https://newmob.github.io/lotx/data/destaque.json'  
 ];
 
 self.addEventListener('install', function(e) {
@@ -28,15 +69,6 @@ self.addEventListener('activate', function(e) {
       }));
     })
   );
-  /*
-   * Fixes a corner case in which the app wasn't returning the latest data.
-   * You can reproduce the corner case by commenting out the line below and
-   * then doing the following steps: 1) load app for first time so that the
-   * initial New York City data is shown 2) press the refresh button on the
-   * app 3) go offline 4) reload the app. You expect to see the newer NYC
-   * data, but you actually see the initial data. This happens because the
-   * service worker is not yet activated. The code below essentially lets
-   * you activate the service worker faster.
-   */
+
   return self.clients.claim();
 });
