e047247a773fabbc71633a668966e375aedc8e1a
newmob <bitspark@gmail.com>
2018-11-19 00:24:41 -0200

    v2.0

--- lotx/js/app.js
+++ lotx/js/app.js
@@ -17,8 +17,13 @@
 
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
+    navigator.serviceWorker.register('../service-worker.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    })
+    .catch(function(error) {
+      console.log('Service worker registration failed, error:', error);
+    });
   }
+
 })();
