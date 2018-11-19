10a12e05d85c9230751d5a5a77cedb852f62f4b7
newmob <bitspark@gmail.com>
2018-11-19 00:58:25 -0200

    v2.0

--- lotx/js/app.js
+++ lotx/js/app.js
@@ -2,7 +2,7 @@
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../service-worker.js')
+    navigator.serviceWorker.register('/lotx/service-worker.js')
     .then(function(registration) {
       console.log('Registration successful, scope is:', registration.scope);
     })
