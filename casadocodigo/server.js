0b469211318d27f61925e83849055ae59917ba9c
newmob <bitspark@gmail.com>
2018-12-08 21:26:00 -0200

    v1.2

--- casadocodigo/server.js
+++ casadocodigo/server.js
@@ -1,16 +1,52 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const servidor = http.createServer(function(req, resp) {
-    resp.end(`
-    <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1>Hello World Node.JS</h1>
-        </body>
-    </html>
-    `);
+app.listen(3000, function() {
+    console.log(`Servidor rodando na porta 3000.`);
+})
+
+app.get('/', function(req, resp) {
+    resp.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Hello World Node.JS</h1>
+            </body>
+        </html>
+        `
+)
 });
 
-servidor.listen(3000,'localhost');
+app.get('/livros', function(req, resp) {
+    resp.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Lista de Livros</h1>
+            </body>
+        </html>
+        `
+)
+});
+
+
+// const http = require('http');
+
+// const servidor = http.createServer(function(req, resp) {
+//     resp.end(`
+//     <html>
+//         <head>
+//             <meta charset="utf-8">
+//         </head>
+//         <body>
+//             <h1>Hello World Node.JS</h1>
+//         </body>
+//     </html>
+//     `);
+// });
+
+// servidor.listen(3000,'localhost');
