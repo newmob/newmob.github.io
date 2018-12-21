ef95e91a5dd001fffee5b3559fd819383b9c8c68
newmob <40585151+newmob@users.noreply.github.com>
2018-12-21 15:07:05 -0200

    v1

--- lotpwa/config/config.js
+++ lotpwa/config/config.js
@@ -6,15 +6,56 @@ if (!env) {
     env = "prod";
 }
 
-console.log(env);
-console.log(configJSON.dev.apiDestaque.endPoint);
-console.log(configJSON.templates.MEGA_SENA);
-
 var config = {};
+config.env = env;
 config.apiDestaque = {};
 config.apiConferirBilhete = {};
 config.templates = {};
-config.errors = {};
+config.errors = [];
+
+if (env == "prod") {
+    try {
+        config.apiDestaque = configJSON.prod.apiDestaque;
+        config.apiConferirBilhete = configJSON.prod.apiConferirBilhete;
+    } catch(err) {
+        config.errors.push("Erros na configuração das APIs de <prod>");
+    }
+} else {
+    try {
+        config.apiDestaque = configJSON.dev.apiDestaque;
+        config.apiConferirBilhete = configJSON.dev.apiConferirBilhete;
+    } catch(err) {
+        config.errors.push("Erros na configuração das APIs de <dev>");
+    }
+}
+
+// remove, se houver, a barra do final dos endPoints
+if (config.apiDestaque.endPoint.endsWith("/")) {
+    config.apiDestaque.endPoint = config.apiDestaque.endPoint.slice(0,-1);
+} 
+
+if (config.apiConferirBilhete.endPoint.endsWith("/")) {
+    config.apiConferirBilhete.endPoint = config.apiConferirBilhete.endPoint.slice(0,-1);
+} 
+
+// acrescenta, se não houver, uma barra no início do resource
+if (!config.apiDestaque.resource.startsWith("/")) {
+    config.apiDestaque.resource = "/" + config.apiDestaque.resource;
+} 
+
+if (!config.apiConferirBilhete.resource.startsWith("/")) {
+    config.apiConferirBilhete.resource = "/" + config.apiConferirBilhete.resource.slice(0,-1);
+} 
+
+// define as propriedades uri
+config.apiDestaque.uri = config.apiDestaque.endPoint + config.apiDestaque.resource;
+config.apiConferirBilhete.uri = config.apiConferirBilhete.endPoint + config.apiConferirBilhete.resource;
+
+// templates
+config.templates = configJSON.templates;
+
+// erros
+
 
 module.exports = config;
 
--- lotpwa/index.js
+++ lotpwa/index.js
@@ -3,5 +3,7 @@ var destaques = require('./controllers/destaques');
 
 
 var config = require('./config/config');
+
+console.log(config);
 //destaques.criaHTML();
 
