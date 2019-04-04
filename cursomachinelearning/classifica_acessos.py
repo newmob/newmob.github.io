d652b243b18882160c8bb1d454ab405aa2bc82f1
Marcelo de Araujo Maximiano <marcelo.maximiano@gmail.com>
2019-04-04 09:59:37 -0300

    v1

--- cursomachinelearning/classifica_acessos.py
+++ cursomachinelearning/classifica_acessos.py
@@ -8,6 +8,12 @@ from sklearn.naive_bayes import MultinomialNB
 modelo = MultinomialNB()
 modelo.fit(X, Y)
 
-teste = [[1,0,1],[0,1]
-resultado = modelo.predict(teste)
+resultado = modelo.predict(X)
+diferencas = resultado - Y
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(X)
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
+print("Taxa de acerto: ",taxa_de_acerto,"%")
+print("Total de elementos: ",total_de_elementos)
--- cursomachinelearning/dados.py
+++ cursomachinelearning/dados.py
@@ -14,3 +14,18 @@ def carregar_acessos():
         Y.append(int(comprou))
 
     return X, Y
+
+def carregar_buscas():
+    X = []    # dados, o que sabemos
+    Y = []    # marcações, o que estamos procurando saber
+
+    arquivo = open('buscas.csv','rt')
+    leitor = csv.reader(arquivo)
+    next(leitor, None)  # pula o cabeçalho
+
+    for home,busca,logado,comprou in leitor:
+        dados = [int(home),busca,int(logado)]
+        X.append(dados)
+        Y.append(int(comprou))
+
+    return X, Y    
